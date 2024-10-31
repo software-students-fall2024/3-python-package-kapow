a5b550d20ffd5aaf5645a198ff6762f35096cf08
seeyeh <will.yn.xie@gmail.com>
2024-10-31 19:01:45 -0400

    Made the string generating into a function called talk()

--- src/pyanimalconverter/conversation.py
+++ src/pyanimalconverter/conversation.py
@@ -1,25 +1,48 @@
 import os
 
+def talk(leftText="",rightText=""):
+    one_speechBubbleTop = '''
+                                            ----------------------------------
+    '''
+    one_speechBubbleBottom = '''
+                                            ---   _---------------------------
+                                              / _/
+                                             //
+    '''
+    middleText = f'''
+              {leftText}                                   {rightText}
+    '''
+    both_speechBubbleTop = '''
+        -----------------------------       ----------------------------------
+    '''
+    both_speechBubbleBottom = '''
+        ----_   ---------------------       ---   _---------------------------
+            \ _\                              / _/
+                \\                           //
+    '''
+    animal = '''
+                        _           _ 
+            __   ___.--'_`.       .'_`--.___   __
+            ( _`.'. -   'o` )     ( 'o`   - .`.'_ )
+            _\.'_'      _.-'       `-._      `_`./_
+            ( \`. )    //\`           '/\\    ( .'/ )
+             \_`-'`---'\\__,         ,__//`---'`-'_/
+              \`        `-\           /-'        '/
+               `                                 '   
+    '''
+    if(leftText==""):
+        return one_speechBubbleTop + middleText + one_speechBubbleBottom + animal
+    else:
+        return both_speechBubbleTop + middleText + both_speechBubbleBottom + animal
+
 def askAnimal():
+    navigation = "menu";
     while(True):
         os.system('clear');
-        animal = '''
-                            __________________________________
-                            |    Ribbet! What do you want?   |
-                            ----------------------------------
-                              / /
-                             / /
-                _         _ / /
-    __   ___.--'_`.     .'_`--.___   __
-    ( _`.'. -   'o` )   ( 'o`   - .`.'_ )
-    _\.'_'      _.-'     `-._      `_`./_
-    ( \`. )    //\`         '/\\    ( .'/ )
-    \_`-'`---'\\__,       ,__//`---'`-'_/
-    \`        `-\         /-'        '/
-    `                               '   
-    '''
-        print(animal);
-        menuInput = input("I want to (CONVERT/COMPARE): ")
+        if(navigation=="menu"):
+            print(talk("","Ribbit, what do you want?"))
+        
+        menuInput = input("I want to (CONVERT/COMPARE) some numbers: ")
         print(menuInput);
         menuInput = input("Continue? (Y/N): ")
         if(menuInput == "N"):
