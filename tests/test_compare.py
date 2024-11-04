0a58adadc2925789a8198e31b84fccebb9fa7c26
mkphung29 <mkp6075@nyu.edu>
2024-11-04 16:02:38 -0500

    Change imports

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -1,10 +1,4 @@
-import sys
-import os
 import pytest
-
-# Add the src directory to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-
 from pyanimalconverter.convert import compare
 
 
