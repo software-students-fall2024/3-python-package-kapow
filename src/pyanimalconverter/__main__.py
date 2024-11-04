ecc6a44ce27e9c2ac81980e421a1866141da594a
StephenS2021 <sspencerwong28@gmail.com>
2024-11-03 19:58:10 -0500

    Created basic compare and convert functions

--- src/pyanimalconverter/__main__.py
+++ src/pyanimalconverter/__main__.py
@@ -3,7 +3,16 @@ from . import convert
 
 
 def main():
-    args = convert.parse_args(sys.argv[1:])
+    args = convert.parse_main_args(sys.argv[1:])
+    if args.operation == 'compare':
+        convert.compare(args.num1, args.num2, args.unit1, args.unit2)
+    elif args.operation == 'convert':
+        convert.convert(args.num1, args.from_unit, args.to_unit)
+    elif args.operation == 'help':
+        pass
+        if args.help_subcommand == "units":
+            convert.print_units()
+    return args
 
 if __name__ == "__main__":
     main()
