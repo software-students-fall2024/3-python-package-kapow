8723f24f4b3d995e08b7bf8cd558a5471991e163
seeyeh <will.yn.xie@gmail.com>
2024-11-04 14:25:38 -0500

    Some extra logic, not finished

--- src/pyanimalconverter/conversation.py
+++ src/pyanimalconverter/conversation.py
@@ -1,6 +1,10 @@
 import os
+from pint import UnitRegistry
 
-def talk(leftText="",rightText=""):
+ureg = UnitRegistry()
+Q_ = ureg.Quantity
+
+def talk(rightText="",leftText=""):
     one_speechBubbleTop = '''
                                             ----------------------------------
     '''
@@ -40,10 +44,24 @@ def askAnimal():
     while(True):
         os.system('clear');
         if(navigation=="menu"):
-            print(talk("","Ribbit, what do you want?"))
-        
-        menuInput = input("I want to (CONVERT/COMPARE) some numbers: ")
-        print(menuInput);
+            print(talk("Ribbit, what do you want?"))
+            menuInput = input("I want to (CONVERT/COMPARE) some numbers: ")
+            print(menuInput);
+            if(menuInput=="CONVERT"):
+                navigation="convert";
+        if(navigation=="convert"):
+            os.system('clear');
+            print(talk("Ribbit, ok, what number do you want to convert?","I want to convert a number!"));
+            validUnit = True;
+            while(True):
+                convertFrom = input("Enter the number with units (e.g. 1 kg, 5 kg) you want to convert: ");
+                try:
+                    convertFrom_num = Q_(convertFrom);
+                except Exception as e:
+                    print(e);
+                else:
+                    os.system('clear');
+                    convertTo = input("")
         menuInput = input("Continue? (Y/N): ")
         if(menuInput == "N"):
             break
